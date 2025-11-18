9dfdbda82d71422d09f58645f5d5000b46b46692
Rusha Shrestha <shrestharusha194@gmail.com>
2025-11-18 14:56:08 +0545

    Refactor login tests to full POM structure with fixture-based credentials.

--- tests/LoginPage.spec.js
+++ tests/LoginPage.spec.js
@@ -1,6 +1,5 @@
 import { expect, test } from "@playwright/test";
 import { LoginPage } from "./pages/LoginPage.js";
-import { CREDENTIALS } from "./fixtures/testData.js";
 
 
 //A: ARRANGE
@@ -11,15 +10,12 @@ let login;
 
 test.beforeEach(async ({ page }) => {
   login = new LoginPage(page);
-  await login.navigate();
+  await login.navigateToLoginPage();
 });
 
 test("Login with valid credentials", async ({ page }) => {
-  await login.login( // A: ACT, fills fields and clicks login.
-    CREDENTIALS.valid.facilityCode,
-    CREDENTIALS.valid.email,
-    CREDENTIALS.valid.password
-  );
+  await login.validLogin(); //Action
+  await login.clickLoginButton();
   // A: ASSERT, confirms login 
   await expect(page).toHaveURL(process.env.BASE_URL);
   console.log("Logged in");
@@ -27,19 +23,17 @@ test("Login with valid credentials", async ({ page }) => {
 
 test("Login with invalid facility code", async ({ page }) => {
     // A: ACT, attempts login with fake facility code.
-  await login.login(CREDENTIALS.invalid.facilityCode, CREDENTIALS.valid.email, CREDENTIALS.valid.password);
-  //A: Assert
+  await login.invalidFacilityCodeLogin();
+  await login.clickLoginButton();
+    //A: Assert
   const toast = page.getByText(/invalid facility code/i);
   await expect(toast).toBeVisible({ timeout: 1000 });
 });
 
 test("Login with invalid email", async ({ page }) => {
     //A: ACT
-  await login.login(
-    CREDENTIALS.valid.facilityCode,
-    CREDENTIALS.invalid.email,
-    CREDENTIALS.valid.password
-  ); 
+  await login.invalidEmailLogin();
+  await login.clickLoginButton();
   //A: ASSERT
   const toast = page.getByText(/user not registered/i);
   await expect(toast).toBeVisible({ timeout: 8000 });
@@ -47,11 +41,8 @@ test("Login with invalid email", async ({ page }) => {
 
 test("Login with invalid password", async ({ page }) => {
     //A: ACT
-  await login.login(
-    CREDENTIALS.valid.facilityCode,
-    CREDENTIALS.valid.email,
-    CREDENTIALS.invalid.password
-  );
+  await login.invalidPasswordLogin();
+  await login.clickLoginButton();
   //A: ASSERT
   const toast = page.getByText(/invalid credentials/i);
   await expect(toast).toBeVisible({ timeout: 15000 });
@@ -59,8 +50,8 @@ test("Login with invalid password", async ({ page }) => {
 
 test("Login with empty credentials", async ({ page }) => {
     //ACT
-  await login.login(CREDENTIALS.empty.facilityCode, CREDENTIALS.empty.email, CREDENTIALS.empty.password);
-  //Assert
+  await login.clickLoginButton();
+    //Assert
   await expect(page.getByText(/facility.*required/i)).toBeVisible({
     timeout: 8000,
   });
@@ -70,11 +61,8 @@ test("Login with empty credentials", async ({ page }) => {
 
 test("test login with SQL injection attempts", async ({ page }) => {
     //Act
-  await login.login(
-    CREDENTIALS.security.sqlInjection.facilityCode,
-    CREDENTIALS.security.sqlInjection.email,
-    CREDENTIALS.security.sqlInjection.password
-  );
+  await login.sqlInjectionLogin();
+  await login.clickLoginButton();
   //Assert
   const toast = page.getByText(
     /value is not a valid email address: An email address must have an @-sign./i
@@ -84,11 +72,7 @@ test("test login with SQL injection attempts", async ({ page }) => {
 
 test("test login with XSS atempt", async ({ page }) => {
     //Act
-  await login.login(
-    CREDENTIALS.security.xss.facilityCode,
-    CREDENTIALS.security.xss.email,
-    CREDENTIALS.security.xss.password
-  );
+  await login.xxsLogin();
   //Assert
   await expect(page).not.toHaveURL(/alert/);
 });
--- tests/pages/LoginPage.js
+++ tests/pages/LoginPage.js
@@ -1,3 +1,5 @@
+import { CREDENTIALS } from "../fixtures/testData";
+
 export class LoginPage {
     constructor(page) {
         this.page = page;
@@ -7,15 +9,44 @@ export class LoginPage {
         this.loginButton = page.getByRole('button', { name: 'Login' });
     }
 
-    async navigate(){
+    async navigateToLoginPage(){
         await this.page.goto(process.env.BASE_URL);
     }
+
+    async clickLoginButton(){
+        await this.loginButton.click();
+    }
     
-    async login(code, email, password) {
+    async FacilityLogin(code, email, password) {
         await this.facilityCode.fill(code);
         await this.email.fill(email);
         await this.password.fill(password);
-        await this.loginButton.click();
+    }
+
+    async validLogin(){
+        await this.FacilityLogin(CREDENTIALS.valid.facilityCode, CREDENTIALS.valid.email, CREDENTIALS.valid.password);
+    }
+
+    async invalidFacilityCodeLogin(){
+        await this.FacilityLogin(CREDENTIALS.invalid.facilityCode, CREDENTIALS.valid.email, CREDENTIALS.valid.password);
+    }
+
+    async invalidEmailLogin(){
+        await this.FacilityLogin(CREDENTIALS.valid.facilityCode, CREDENTIALS.invalid.email, CREDENTIALS.valid.password);
+    }
+
+    async invalidPasswordLogin(){
+        await this.FacilityLogin(CREDENTIALS.valid.facilityCode, CREDENTIALS.valid.email, CREDENTIALS.invalid.password);
+    }
+
+
+
+    async sqlInjectionLogin(){
+        await this.FacilityLogin(CREDENTIALS.security.sqlInjection.facilityCode, CREDENTIALS.security.sqlInjection.email, CREDENTIALS.security.sqlInjection.password);
+    }
+
+    async xxsLogin(){
+        await this.FacilityLogin(CREDENTIALS.security.xss.facilityCode, CREDENTIALS.security.xss.email, CREDENTIALS.security.xss.password);
     }
 
     
